5000475fc37c37de200e793ae5daae65a6697d42
s.matveeva <s.matveeva@corp.mail.ru>
2022-04-29 03:15:10 +0300

    logo&fonts added

---
+++ styles/common.js
@@ -0,0 +1,71 @@
+export const header = {
+    fontSize: 18,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '20px 5px',
+    color: '#0072ff',
+    fontFamily: 'VK Sans Display',
+    fontWeight: 500,
+};
+
+export const coin = {
+    width: '16px',
+    height: '16px',
+}
+
+export const itemCard = {
+    height: 500,
+    title: {
+        fontSize: 32,
+        marginBottom: 20,
+        display: 'flex',
+        justifyContent: 'space-between',
+        fontFamily: 'VK Sans Display',
+        fontWeight: 500,
+    },
+    description: {
+        fontSize: 16,
+        lineHeight: 1.2,
+        fontFamily: 'VK Sans Text',
+        fontWeight: 300,
+    },
+    price: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: '20px 0',
+        fontSize: 16,
+        fontFamily: 'VK Sans Display',
+        fontWeight: 'bold',
+    },
+    button: {
+        fontSize: 30,
+        fontFamily: 'VK Sans Display',
+        fontWeight: 500,
+    },
+    content: {
+        margin: 12,
+    }
+};
+
+export const smallCard = {
+    padding: 12,
+    title: {
+        fontFamily: 'VK Sans Display',
+        fontWeight: 400,
+    },
+    image: {
+        maxHeight: 200,
+    }
+}
+
+export const form = {
+    label: {
+        fontFamily: 'VK Sans Display',
+        fontWeight: 400,
+        fontSize: 16,
+        marginBottom: 8,
+        color: '#808080',
+    }
+}
